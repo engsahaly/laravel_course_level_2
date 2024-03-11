0bfd1c31e129fc12de4776849bd78c8feb1bff7e
engsahaly <engsahaly@gmail.com>
2024-03-11 22:52:52 +0200

    laravel error pages

---
+++ resources/views/errors/400.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', 'Bad Request')
+@section('code', '400')
+@section('message', 'Bad Request')
---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::layout')
+
+@section('title', '403 Custom Title')
+{{-- @section('code', '403') --}}
+@section('message', '403 Custom message')
